30ea4e550c2294f6fcec77d380baa3e135b26a2c
Neo Gullberg <neogullberg04@gmail.com>
2022-02-24 18:24:39 +0100

    ye

--- script.js
+++ script.js
@@ -7,9 +7,8 @@ let column = 0
 let guess = ""
 let win = false
 
-const words = ["horse", "train", "paste", "tooth"]
+const words = ["horse", "train", "paste", "tooth", "fruit", "eight", "skate", "human", "climb", "chair"]
 const answer = words[Math.floor(Math.random()*words.length)].toUpperCase()
-console.log(answer) // remove
 
 function getRow() {
     return wordLines.childNodes[1 + row * 2]
@@ -18,10 +17,13 @@ function getColumn() {
     return wordLines.childNodes[1 + row * 2].childNodes[1 + column * 2]
 }
 
+
 function checkGuess(guess, answer) {
     for (let i = 0; i < 5; i++) {
         if (guess[i] === answer[i]) {
             wordLines.childNodes[1 + row * 2].childNodes[1 + i * 2].style.backgroundColor = "green"
+            let answerWord = document.querySelector("h2").innerText
+            document.querySelector("h2").innerText = answerWord.substring(0, i) + guess[i] + answerWord.substring(i + 1) // this replaces an '_' in the h2 element at index 'i' with the correct character
         } else if (answer.includes(guess[i])) {
             wordLines.childNodes[1 + row * 2].childNodes[1 + i * 2].style.backgroundColor = "yellow"
         }
@@ -48,14 +50,13 @@ document.body.onkeydown = function(event) {
         } else if (keypress === "ENTER") {
             if (column === 5) {
                 checkGuess(guess, answer)
-                console.log("n")
                 row += 1
                 column = 0
                 guess = ""
             }
 
             if (row === 6 && win === false) {
-                document.querySelector("h1").innerText = "You Lost"
+                document.querySelector("h1").innerText = "You Lost :("
             }
         } else if (keypress === "BACKSPACE") {
             if (column > 0) {
